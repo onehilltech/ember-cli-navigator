dabae4efc88edb8f9b77b3f6352a7ce7c5d9d9c6
James Hill <hilljh82@gmail.com>
2024-08-18 17:11:44 -0400

    chore: updated to use capacitor

--- addon/services/geolocation.js
+++ addon/services/geolocation.js
@@ -1,5 +1,5 @@
 import Service from '@ember/service';
-import { getOwner } from '@ember/application';
+import { Geolocation } from '@capacitor/geolocation';
 
 /**
  * Wrapper class for the watch id returned from navigator.geolocation.watchPosition. The
@@ -11,54 +11,36 @@ class PositionWatcher {
   }
 
   clear () {
-    navigator.geolocation.clearWatch (this.watchId);
+    Geolocation.clearWatch ({ id: this.watchId });
   }
 }
 
 export default class GeolocationService extends Service {
-  constructor () {
-    super (...arguments);
-
-    let config = getOwner (this).resolveRegistration ('config:environment');
-
-    if (config.CORBER) {
-      this._configured = this._configureForMobileDevice ();
-    }
-    else {
-      this._configured = Promise.resolve ();
-    }
-  }
-
-  _configureForMobileDevice () {
-    let cordovaEvents = getOwner (this).lookup ('service:ember-cordova/events');
-
-    return new Promise ((resolve) => {
-      cordovaEvents.on ('deviceready', this, function () {
-        resolve ();
-      });
-    });
-  }
-
   /**
    * Get the current position.
    *
-   * @param opts
+   * @param options
    */
-  getCurrentPosition (opts) {
-    return this._configured.then (() => new Promise ((resolve, reject) => navigator.geolocation.getCurrentPosition (resolve, reject, opts)));
+  async getCurrentPosition (options) {
+    return Geolocation.getCurrentPosition (options);
   }
 
   /**
    * Watch for changes to the current position.
    *
-   * @param success
-   * @param failure
-   * @param opts
+   * @param callback
+   * @param options
    */
-  watchPosition (success, failure, opts) {
-    return this._configured.then (() => {
-      let watchId = navigator.geolocation.watchPosition (success, failure, opts);
-      return new PositionWatcher (watchId);
-    });
+  async watchPosition (options, callback) {
+    const watchId = await Geolocation.watchPosition (options, callback);
+    return new PositionWatcher (watchId);
+  }
+
+  async requestPermissions (permissions) {
+    return Geolocation.requestPermissions ({ permissions });
+  }
+
+  async checkPermissions () {
+    return Geolocation.checkPermissions ();
   }
 }
